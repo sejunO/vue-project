d076926df189434ab736c292a38d6d01c76f2c3b
eunchae-cho <eunchae-cho@trycatch.kr>
2022-08-22 21:50:00 +0900

    make todoList

---
+++ client/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="col-md-6 offset-md-3">
+        <h1 class="text-center mb-4">To-Do-List</h1>
+        
+        <input type="text" class="form-control" v-model="userInput" @keyup.enter="addNewTodo">
+
+        <div class="list-group">
+          <button class="list-group-item text-left" v-for="todo in todoList" :key="todo">
+            {{ todo }}
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      userInput: '',
+      todoList: []
+    };
+  },
+  methods: {
+    addNewTodo() {
+      this.todoList.push(this.userInput);
+      this.userInput = '';
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
