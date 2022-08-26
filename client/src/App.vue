fc6359ea04130979b4a98a75892f74888064a5ad
eunchae-cho <dmsco8981@gmail.com>
2022-08-26 22:43:16 +0900

    ,

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,25 +3,21 @@
       <div class="container py-4">
 
         <div class="p-5 mb-4 rounded-3">
-            <div class="col-md-6 offset-md-3">
-              <h1 class="text-center mb-4">To-Do-List</h1>
-                <div class="row"> 
-                  <div class="col-2 plusIcon">
-                    <i class="bi bi-plus-square clickPlusIcon" @click="addNewTodo">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
-                      <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                      <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                    </svg>
-                    </i>
-                  </div>
-                    <div class="col-10">
-                    <input type="text" class="form-control" v-model="userInput" @keyup.enter="addNewTodo">
-                  </div>
-                </div>
-                <div class="list-group list">
-                  {{ errorMessage }}
-                </div>
+          <div class="col-md-6 offset-md-3">
+            <h1 class="text-center mb-4">To-Do-List</h1>
+              <div class="input-group w-100">
+              <span class="input-group-text" id="basic-addon1" @click="addNewTodo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+                  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                </svg>
+              </span>
+              <input type="text" class="form-control" placeholder="할 일을 입력해주세요 :-)" v-model="userInput" @keyup.enter="addNewTodo">
+              </div>
+              <div class="list-group list">
+              {{ errorMessage }}
             </div>
+          </div>
         </div>
 
         <div class="row align-items-md-stretch eachTodo">
@@ -43,6 +39,12 @@
                   <button class="list-group-item text-left mb-4" v-for="todo in ecTodoList" :key="todo" @click="toggleTodoState(todo)">
                   {{ todo.label }}
                   </button>
+                  <!-- <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                      <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+                    </svg>
+                  </div> -->
                 </div>
               <!-- <button class="btn btn-outline-light" type="button">Example button</button> -->
             </div>
@@ -138,9 +140,9 @@ export default {
   padding-right: 0;
 }
 
-.plusIcon {
-  text-align: right;
-  align-self: center;
+#basic-addon1:hover {
+  cursor: pointer;
+  opacity: 0.9;
 }
 
 .list {
