ba741065231037f48c2bde61f694ce7e07dfc47a
eunchae-cho <dmsco8981@gmail.com>
2022-09-11 23:14:00 +0900

    create rest api test

--- server/src/main/java/com/todo/rest/TestApiController.java
+++ server/src/main/java/com/todo/rest/TestApiController.java
@@ -1,4 +1,17 @@
 package com.todo.rest;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
 public class TestApiController {
+
+    @GetMapping("test")
+    public ResponseEntity apiTest() {
+        return ResponseEntity.ok("api response");
+    }
 }
