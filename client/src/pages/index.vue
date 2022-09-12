c5c49514fed411ce619925f7439d92e3e08afbff
eunchae-cho <dmsco8981@gmail.com>
2022-09-12 22:17:36 +0900

    installed vue-router, vuex

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,38 +1,38 @@
-<template>
-    <div>
-    </div>
-</template>
+  <template>
+      <div>
+      </div>
+  </template>
 
 
-<script>
-    import apiRequest from 'util/axios';
+  <script>
+      import apiRequest from 'util/axios';
 
-    export default {
-    name: `test`,
-    components: {
-        TList,
-        TBox,
-        TBoxTitle
-    },
-    data() {
-        return {
-        }
-    },
-    methods: {
-        async testAxios () {
-            try {
-                const response = await apiRequest.test.getName();
-                if(response.success) {
-                    // 내용
-                }
-            } catch (e) {
-                console.error(e)
-            }
-      
-    }
-}
+      export default {
+      name: `test`,
+      components: {
+          TList,
+          TBox,
+          TBoxTitle
+      },
+      data() {
+          return {
+          }
+      },
+      methods: {
+          async testAxios () {
+              try {
+                  const response = await apiRequest.test.getName();
+                  if(response.success) {
+                      // 내용
+                  }
+              } catch (e) {
+                  console.error(e)
+              }
 
-</script>
+      }
+  }
 
-<style scoped>
-</style>
+  </script>
+
+  <style scoped>
+  </style>
